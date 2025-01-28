1dd11cd915e78627bc68c2faf328d7f640fb3aeb
omarelk17 <omarelkhodary2005@gmail.com>
2025-01-28 10:57:13 -0600

    second

---
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+using Ninject;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel mykernel;
+
+        public NinjectDependencyResolver (IKernel kernelParam)
+        {
+            mykernel = kernelParam;
+            AddBinding();
+
+        }
+        public object GetService(Type myserviceType)
+        {
+            return mykernel.GetService(myserviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type myserviceType)
+        {
+            return mykernel.GetAll(myserviceType);
+        }
+        public void AddBinding()
+        {
+            //we need to put binding here
+        }
+
+    }
+}
